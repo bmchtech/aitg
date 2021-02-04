1a7ea5658eac6a32892f2e3322743d85ce4b1e93
xdrie <xdrie@alt.icu>
2021-02-03 20:36:09 -0800

    error on invalid model path

--- src/aitg_host/model.py
+++ src/aitg_host/model.py
@@ -1,6 +1,10 @@
 from aitextgen import aitextgen
+from os.path import isdir
 
 def load_model(path, optimize):
+    if not isdir(path):
+        raise OSError(f'model path is not a valid directory: {path}')
+
     # load model
     ai = aitextgen(model=path+'/pytorch_model.bin', config=path+'/config.json')
 
