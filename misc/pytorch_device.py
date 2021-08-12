0018f4d43715dacff85c233b86fee4a9e5eba153
xdrie <xdrie@alt.icu>
2021-08-12 15:51:16 -0700

    try testing gpu support

---
+++ misc/pytorch_device.py
@@ -0,0 +1,7 @@
+import torch
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("Running on the GPU")
+else:
+    device = torch.device("cpu")
+    print("Running on the CPU")
