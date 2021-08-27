33e571b4fc378f9cc955e84fcc02186a99d98ece
xdrie <xdrie@alt.icu>
2021-08-26 17:35:23 -0700

    input ids simplify

--- src/aitg_host/util.py
+++ src/aitg_host/util.py
@@ -8,7 +8,7 @@ def count_tokens(ai, text):
     return len(str_to_ids(ai, text))
 
 def str_to_ids(ai, text):
-    return ai.tokenizer(text=text)['input_ids']
+    return ai.tokenizer(text=text).input_ids
 
 def ids_to_toks(ai, ids, skip_special_tokens=True):
     return ai.tokenizer.convert_ids_to_tokens(ids, skip_special_tokens=skip_special_tokens)
