ef81367ecd87bb64e78340772b447939e7235348
xdrie <xdrie@alt.icu>
2021-08-28 11:53:21 -0700

    fix gitignore

---
+++ src/aitg_host/models/bart_classifier.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, logging
+
+
+class BartClassifierAI:
+    def __init__(self, model_folder: str, to_device, verbose=False):
+        self.model_folder = model_folder
+        self.device = to_device
+
+        if not verbose:
+            logging.set_verbosity_warning()
+
+        # load model
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_folder, local_files_only=True
+        )
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_folder, local_files_only=True
+        ).to(to_device)
+
+        self.context_window = 1024
