d4f9ed866e3ad83e5d69c4f445812c3d1a0af7c5
redthing1 <redthing1@alt.icu>
2022-07-28 00:29:07 -0700

    basic gptj onnx

---
+++ misc/gptj_onnx.py
@@ -0,0 +1,34 @@
+from transformers import AutoTokenizer
+from optimum.onnxruntime import ORTModelForCausalLM
+import torch
+import typer
+
+def cli(
+    model_path: str
+):
+    print(f"loading model from {model_path}")
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = ORTModelForCausalLM.from_pretrained(model_path)
+    print(f"loaded model")
+
+    # inputs = tokenizer("My name is Philipp and I live in Germany.", return_tensors="pt")
+    # gen_tokens = model.generate(
+    #     **inputs, do_sample=True, temperature=0.9, min_length=20, max_length=20
+    # )
+    # tokenizer.batch_decode(gen_tokens)
+    
+    user_input = input("prompt: ")
+    while user_input.strip() != "":
+        # do inference
+        inputs = tokenizer(user_input, return_tensors="pt")
+        gen_tokens = model.generate(
+            **inputs, do_sample=True, temperature=0.9, max_length=32
+        )
+        print(tokenizer.batch_decode(gen_tokens))
+        user_input = input("prompt: ")
+
+def main():
+    typer.run(cli)
+
+if __name__ == "__main__":
+    main()
